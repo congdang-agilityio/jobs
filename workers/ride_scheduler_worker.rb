8858694415fd4055b9b35133d225021043b156d3
Hai Phan <hai.phanthanh@asnet.com.vn>
2017-02-20 18:19:40 +0700

    send more param to ride worker

--- workers/ride_scheduler_worker.rb
+++ workers/ride_scheduler_worker.rb
@@ -34,7 +34,7 @@ class RideSchedulerWorker
         # Make a ride request
         if estimated.present?
           ride_request = Ridesharing::RideRequest.new ENV['RIDESHARING_RIDE_EXCHANGE']
-          ride_response, ride_error = ride_request.call ride_params(params.merge(estimated.slice(:vendor, :car_type)))
+          ride_response, ride_error = ride_request.call ride_params(params.merge(estimated))
           logger.info("Ride Response: \n\tRESPONSE: #{ride_response}\n\tERROR: #{ride_error}")
           if ride_response.present?
             # TODO: push to webhook
@@ -80,6 +80,11 @@ class RideSchedulerWorker
       :destination_longitude,
       :car_type,
       :ride_request_id,
+      :vendor_car_name,
+      :car_image_url,
+      :price_base,
+      :min_time_estimate,
+      :min_price_estimate,
       :access_token,
       :webhook_push)
   end
